44f147e434a44045c93cf7d4eed200f0f06bd19a
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-11-10 22:07:22 +0530

    Update __main__.py

--- DAXXROBOT/__main__.py
+++ DAXXROBOT/__main__.py
@@ -435,7 +435,7 @@ def Mukesh_about_callback(update: Update, context: CallbackContext):
                             text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ", url=f"tg://user?id={OWNER_ID}"
                         ),
                         InlineKeyboardButton(
-                            text="ɢɪᴛʜᴜʙ 🍹", url="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x69\x74\x68\x75\x62\x2E\x63\x6F\x6D\x2F\x6E\x6F\x6F\x62\x2D\x6D\x75\x6B\x65\x73\x68"
+                            text="ɢɪᴛʜᴜʙ 🍹"68747470733A2F2F6769746875622E636F6D2F444158585445414D"
                         ),
                     ],
                     [
