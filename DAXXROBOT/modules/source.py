b318e0b5cb91949f9f6f3f3837f82c8417a86467
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-12 09:56:39 +0530

    Update source.py

--- DAXXROBOT/modules/source.py
+++ DAXXROBOT/modules/source.py
@@ -9,13 +9,13 @@ from telethon import __version__ as s
 from DAXXROBOT import OWNER_ID, dispatcher
 from DAXXROBOT import pbot as client
 
-Mukesh = "https://te.legra.ph/file/1a72f3770dcb90ee8b3f7.jpg"
+DAXX = "https://graph.org/file/22f4ce50778692e31a3b8.jpg"
 
 
 @client.on_message(filters.command(["repo", "source"]))
 async def repo(client, message):
     await message.reply_photo(
-        photo=Mukesh,
+        photo=DAXX,
         caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
 
 **» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [ᴅᴇᴠᴇʟᴏᴘᴇʀ](tg://user?id={OWNER_ID})
@@ -34,7 +34,7 @@ async def repo(client, message):
                     ),
                     InlineKeyboardButton(
                         "• ʀᴇᴘᴏ •",
-                        url="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x69\x74\x68\x75\x62\x2E\x63\x6F\x6D\x2F\x4E\x6F\x6F\x62\x2D\x4D\x75\x6B\x65\x73\x68\x2F\x4D\x75\x6B\x65\x73\x68\x52\x6F\x62\x6F\x74",
+                        url="https://t.me/daxxsir3",
                     ),
                 ]
             ]
