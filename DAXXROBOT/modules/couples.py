affaaa792b1ec92c43778503020cb401d23782bf
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-11-12 10:26:08 +0530

    Update couples.py

--- DAXXROBOT/modules/couples.py
+++ DAXXROBOT/modules/couples.py
@@ -90,8 +90,8 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (434, 218), img1)
-         img.paste(img2, (994, 218), img2)
+         img.paste(img1, (434, 217), img1)
+         img.paste(img2, (994, 217), img2)
 
          img.save(f'test_{cid}.png')
     
