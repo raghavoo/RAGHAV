9a49ae8960791bfd5b67c8e3660381d2b4031e8e
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-15 22:59:13 +0530

    Update alive.py

--- DAXXROBOT/modules/alive.py
+++ DAXXROBOT/modules/alive.py
@@ -17,7 +17,7 @@ PHOTO = [
     "https://telegra.ph/file/bdfd86195221e979e6b20.jpg",
 ]
 
-Mukesh = [
+DAXX = [
     [
         InlineKeyboardButton(text="ɴᴏᴏʙ", user_id=OWNER_ID),
         InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
@@ -64,5 +64,5 @@ async def restart(client, m: Message):
   
   » **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{pyver()}`
    ━━━━━━━━━━━━━━━━━━━""",
-        reply_markup=InlineKeyboardMarkup(Mukesh),
+        reply_markup=InlineKeyboardMarkup(DAXX),
     )
